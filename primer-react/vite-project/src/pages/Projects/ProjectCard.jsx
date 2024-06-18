3b5ed0eadec4fccd86ff15afcfcf0cacf54670e0
mariva0280 <marivafp@gmail.com>
2024-06-18 18:52:27 +0200

    Change in projectCard

--- primer-react/vite-project/src/pages/Projects/ProjectCard.jsx
+++ primer-react/vite-project/src/pages/Projects/ProjectCard.jsx
@@ -1,24 +1,32 @@
-import React from "react"
-import styles from './ProjectCardStyles.module.css'
+import styles from './ProjectsStyles.module.css'
+import PropTypes from 'prop-types'
 
-function ProjectCard({ src, link, title, shortDescription, summary, isEven }) {
-  return (
-    <div className={`${styles.projectCard} ${isEven ? styles.even : styles.odd}`}>
-      <a href={link} target='_blank' rel='noopener noreferrer' className={styles.imageWrapper}>
-        <img className={`${styles.projectImage} hover`} src={src} alt={`${title} logo`} />
-      </a>
-      <div className={styles.projectDetails}>
-        <h3 className={styles.projectTitle}>{title}</h3>
-        <p className={styles.shortDescription}>{shortDescription}</p>
-        <p className={styles.summary}>{summary}</p>
-      </div>
-    </div>
-  );
+function ProjectCard({ src, link, title, shortDescription, summary, isEven }){
+    return (
+        <div className={`${styles.projectCard} ${isEven ? styles.even : styles.odd}`}>
+            <a href={link} target='_blank' className={styles.imageWrapper}>
+                <img className={`${styles.projectImage} hover`} src={src} alt={`${title} logo`}/>
+            </a>
+            <div className={styles.projectDetails}>
+                <h3 className={styles.projectTitle}>{title}</h3>
+                <p className={styles.shortDescription}>{shortDescription}</p>
+                <p className={styles.summary}>{summary}</p>
+            </div>
+        </div>        
+    )
+}
+
+ProjectCard.propTypes = {
+    src: PropTypes.string.isRequired,
+    link: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    shortDescription: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    isEven: PropTypes.bool.isRequired,
+
 }
 
 export default ProjectCard
-
-
 
 
 
--- primer-react/vite-project/src/pages/Projects/Projects.jsx
+++ primer-react/vite-project/src/pages/Projects/Projects.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import styles from './ProjectsStyles.module.css'
 import projectData from './ProjectData'
 import ProjectCard from './ProjectCard'
